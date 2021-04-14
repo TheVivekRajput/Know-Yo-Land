bb45dceecd72d496239df7b4b4e27202863c914d
Hub-Abhi <62337400+Hub-Abhi@users.noreply.github.com>
2021-04-14 07:25:35 +0530

    Add files via upload

---
+++ scrollup.js
@@ -0,0 +1,19 @@
+/*Scroll to top when arrow up clicked BEGIN*/
+$(window).scroll(function() {
+    var height = $(window).scrollTop();
+    if (height > 200) {
+        $('#back2Top').fadeIn();
+    } else {
+        $('#back2Top').fadeOut();
+    }
+});
+$(document).ready(function() {
+    $("#back2Top").click(function(event) {
+        event.preventDefault();
+        // $("html, body").animate({ scrollTop: 0 }, "slow");
+        $("html").animate({ scrollTop: 0 }, 100);
+        return false;
+    });
+
+});
+ /*Scroll to top when arrow up clicked END*/
